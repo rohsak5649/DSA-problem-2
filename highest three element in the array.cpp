42c0be72fe4ee913c58c7aac9ce111ce9451af3f
Rohan Sakhare <126242412+rohsak5649@users.noreply.github.com>
2024-04-29 19:17:48 +0530

    Create highest three element in the array.cpp

---
+++ highest three element in the array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int n;
+    cout<<"Enetr the size of the array";
+    cin>>n;
+    
+    int arr[n];
+    for(int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+        
+    }
+    int a= arr[0], b= arr[1] ,c= arr[2];
+    
+    for(int i = 0 ; i <n; i++){
+        if (arr[i]>a){
+            c = b ;
+            b = a;
+            a = arr[i];
+        }
+        else if (arr[i]>b){
+            c = b;
+            b = arr[i];
+        }
+        else if (arr[i]>c){
+            c= arr[i];
+        }
+    }
+    cout<<a<<","<<b<<","<<c;
+    
+}
